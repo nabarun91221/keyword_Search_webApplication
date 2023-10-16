920d4f02ea5f1ae6b122a48ad6cf774a2b43877b
Nabarun Middya <72463614+nabarun91221@users.noreply.github.com>
2023-10-16 16:36:57 +0530

    Update DropZone.js

--- client/src/components/DropZone.js
+++ client/src/components/DropZone.js
@@ -49,8 +49,12 @@ const Dropzone = ({
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    if (files.length > 0) {
+    if (files.length > 6) {
+      noty_handleClick(
+        "uploading more than 6 files is not acceptable, this is to prevent memory leaks"
+      );
+    }
+    if (files.length > 0 && files.length <= 6) {
       setProcessData([]);
       let formData = new FormData();
       files.forEach((file) => {
@@ -101,15 +105,15 @@ const Dropzone = ({
         <h3 className="title text-lg font-semibold text-neutral-600 mt-10 border-b pb-3">
           Accepted Files
         </h3>
-        <ul className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-10">
+        <ul className="mt-6 pb-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-10">
           {files.length > 0 &&
             files.map((file) => (
               <li
                 key={file.name}
-                className="relative h-32 rounded-md shadow-lg"
+                className="relative h-20 w-16 rounded-md shadow-lg"
               >
                 <Image
-                  src="/document.png"
+                  src="/doc.png"
                   alt={file.name}
                   width={100}
                   height={100}
@@ -138,7 +142,7 @@ const Dropzone = ({
       <input
         type="submit"
         value="PROCESS FILES"
-        className="w-full mb-5 mt-10 h-10 border border-secondary-400 bg-secondary-400 rounded flex justify-center items-center transition-colors"
+        className="w-full mb-5 mt-20 h-10 border border-secondary-400 bg-secondary-400 rounded flex justify-center items-center transition-colors hover:bg-white hover:text-black"
       ></input>
     </form>
   );
